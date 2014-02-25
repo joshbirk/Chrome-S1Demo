6696a126cb5aa0abca2de83530748266e7da02f5
joshbirk <joshua.birk@salesforce.com>
2014-02-25 13:25:32 -0600

    minor changes to manifest and background.js

--- S1Demo/background.js
+++ S1Demo/background.js
@@ -13,10 +13,7 @@ chrome.pageAction.onClicked.addListener(function() {
    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
 		   function(tabs){
 		   		url = tabs[0].url;
-		      	chrome.windows.create({'url': 'demo.html', 'type': 'popup', 'height':750, 'width':360}, function(window) {
-   				/*	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    					chrome.tabs.sendMessage(tabs[0].id, {url: url}, function(response) {});  
-					}); */
+		      	chrome.windows.create({'url': 'demo.html', 'type': 'popup', 'height':750, 'width':360, 'left': 350}, function(window) {
    				});   
 		   });
 });
--- S1Demo/content.js
+++ S1Demo/content.js
@@ -3,14 +3,5 @@ window.onload = function() {
     host = url.split('.com/')[0];
     host = host + '.com';
     window.location.href = host + '/one/one.app';
-/*
-    $.getScript( host + '/jslibrary/0000/sfdc/SfdcCore.js', function() {
-        $.getScript( host + '/jslibrary/0000/sfdc/AuraAlohaFrameNavigator.js', function() {
-
-            console.log(sforce.one.navigateToSObject);
-
-        });
-    
-    });    */
 }
 
